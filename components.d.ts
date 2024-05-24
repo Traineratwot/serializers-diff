78a9d38281267c8a5e34a0befb47a0b55af61679
kirill <traineratwot@yandex.ru>
2024-05-24 23:49:56 +0300

    init

--- components.d.ts
+++ components.d.ts
@@ -7,20 +7,12 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    Avsc: typeof import('./src/components/Avsc.vue')['default']
-    Csv: typeof import('./src/components/Csv.vue')['default']
     Fieldset: typeof import('primevue/fieldset')['default']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     Ini: typeof import('./src/components/Ini.vue')['default']
     Json: typeof import('./src/components/Json.vue')['default']
     Json5: typeof import('./src/components/Json5.vue')['default']
-    Msgpack5: typeof import('./src/components/Msgpack5.vue')['default']
-    Protobuf: typeof import('./src/components/Protobuf.vue')['default']
-    Splitter: typeof import('primevue/splitter')['default']
-    SplitterPanel: typeof import('primevue/splitterpanel')['default']
     Textarea: typeof import('primevue/textarea')['default']
     Toml: typeof import('./src/components/Toml.vue')['default']
-    Xml: typeof import('./src/components/Xml.vue')['default']
     Yaml: typeof import('./src/components/Yaml.vue')['default']
   }
 }
